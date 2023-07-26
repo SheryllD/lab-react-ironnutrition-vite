7e08e8de76e014494c9c85f92d1fa5eb5093ee38
Sheryll Dumapal <sheryllhhs@outlook.com>
2023-07-26 14:36:57 +0200

    added iteration 1 and 2

--- src/components/FoodBox.jsx
+++ src/components/FoodBox.jsx
@@ -1,1 +1,14 @@
-// Your code here
+<div>
+  <p>NAME</p>
+
+  <img src="IMAGE" />
+
+  <p>Calories: CALORIES</p>
+  <p>Servings SERVINGS</p>
+
+  <p>
+    <b>Total Calories: SERVINGS * CALORIES </b> kcal
+  </p>
+
+  <button>Delete</button>
+</div>
